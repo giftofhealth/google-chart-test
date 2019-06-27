4b28bfb6054cae44df4097fb93991b2f506e64d8
Rajesh Vaidheeswarran <rv@gnu.org>
2019-06-26 23:48:21 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartReadyEvent, ChartErrorEvent, ChartSelectEvent,
+	 ChartMouseOverEvent, ChartMouseOutEvent } from 'ng2-google-charts';
+import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces';
+
+declare var $: any;
+declare var google: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'google-chart-test';
+    public selectEvent: ChartSelectEvent;
+    title = 'google-chart-test';
+    chart_ready = false;
+    public pieChart: GoogleChartInterface = {
+	chartType: 'PieChart',
+	dataTable: [
+	    ['Task', 'Hours per Day'],
+	    ['Work',     11],
+	    ['Eat',      2],
+	    ['Commute',  2],
+	    ['Watch TV', 2],
+	    ['Sleep',    7]
+	],
+	//opt_firstRowIsData: true,
+	options: {'title': 'Tasks'},
+    };
+
+    public tableChart =  {
+	chartType: 'Table',
+	dataTable: [
+	    ['Department', 'Revenues', 'Another column', 'ColorFormat'],
+	    ['Shoes', 10700, -100, 100],
+	    ['Sports', -15400, 25, 500],
+	    ['Toys', 12500, 40, 800],
+	    ['Electronics', -2100, 889, 1000],
+	    ['Food', 22600, 78, 1100],
+	    ['Art', 1100, 42, 400]
+	],
+	formatters: [
+	    {
+		columns: [1, 2],
+		type: 'NumberFormat',
+		options: {
+		    prefix: '&euro;', negativeColor: 'red', negativeParens: true
+		}
+	    },
+	    {
+		columns: [3],
+		type: 'ColorFormat',
+		options: {
+		    ranges: [
+			{from: 100, to: 900, fromBgColor: 'green', toBgColor: 'yellow'}
+		    ]
+		}
+	    }
+	],
+	options: {title: 'Countries', allowHtml: true}
+    };
+  public ready(event: ChartReadyEvent) {
+    console.log(event.message);
+  }
+
+  public error(event: ChartErrorEvent) {
+    console.error("Error: " + event);
+  }
+
+  public select(event: ChartSelectEvent) {
+      this.selectEvent = event;
+  }
+
+  public mouseOver(event: ChartMouseOverEvent) {
+    console.log('ChartMouseOverEvent');
+    console.log('bb: ' + JSON.stringify(event.boundingBox));
+    console.log('pos: ' + JSON.stringify(event.position));
+    console.log('type: ' + JSON.stringify(event.columnType));
+    console.log('label: ' + JSON.stringify(event.columnLabel));
+    console.log('value: ' + JSON.stringify(event.value));
+  }
+
+  public mouseOut(event: ChartMouseOutEvent) {
+    console.log('ChartMouseOutEvent');
+    console.log('bb: ' + JSON.stringify(event.boundingBox));
+    console.log('pos: ' + JSON.stringify(event.position));
+    console.log('type: ' + JSON.stringify(event.columnType));
+    console.log('label: ' + JSON.stringify(event.columnLabel));
+    console.log('value: ' + JSON.stringify(event.value));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+	AppComponent
+    ],
+    imports: [
+	BrowserModule,
+	Ng2GoogleChartsModule
+    ],
+    providers: [
+	//{provide: 'googleChartsVersion', useValue: '46'},
+	//{provide: 'mapsApiKey', useValue: 'MYGOOGLEMAPSAPIKEY'}
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
